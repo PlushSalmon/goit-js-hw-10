3d50149956ffae57f28f49733cfbd218889e11ae
PlushSalmon <117682267+PlushSalmon@users.noreply.github.com>
2023-02-14 14:27:24 +0200

    Final

--- src/index.js
+++ src/index.js
@@ -1,57 +1,43 @@
-// import './css/styles.css';
-// import { fetchCountries } from './js/fetchCountries';
-// import debounce from 'lodash.debounce';
-// import Notiflix from 'notiflix';
-
-// const DEBOUNCE_DELAY = 300;
-// const input = document.getElementById('search-box');
-// const list = document.querySelector('.country-list');
-// const info = document.querySelector('.country-info');
-
-// input.addEventListener("input",)
-
-
-// ------------------
-
+// -----IMPORT-----
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
+// -----CONSTS&LINKS-----
 const DEBOUNCE_DELAY = 300;
-
 const inputEl = document.getElementById('search-box');
 const listEl = document.querySelector('.country-list');
 const infoEl = document.querySelector('.country-info');
 
-const cleanMarkup = ref => (ref.innerHTML = '');
+// -----FUNCTIONS-----
+function cleanMarkup(ref) {
+    ref.innerHTML = ''
+}
 
-const inputHandler = e => {
+function inputHandler(e) {
     const textInput = e.target.value.trim();
-
     if (!textInput) {
         cleanMarkup(listEl);
         cleanMarkup(infoEl);
         return;
     }
-
-    fetchCountries(textInput)
-        .then(data => {
-            console.log(data);
-            if (data.length > 10) {
-                Notiflix.Notify.info('Too many matches found. Please enter a more specific name', { fontSize: '15px', },);
-                return;
-            }
-            renderMarkup(data);
-        })
-        .catch(err => {
+    fetchCountries(textInput).then(data => {
+        console.log(data);
+        if (data.length > 10) {
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name', { fontSize: '15px', },);
+            return;
+        }
+        renderMarkup(data);
+    })
+        .catch(error => {
             cleanMarkup(listEl);
             cleanMarkup(infoEl);
             Notiflix.Notify.failure('Oops, there is no country with that name', { fontSize: '15px', },);
         });
-};
+}
 
-const renderMarkup = data => {
+function renderMarkup(data) {
     if (data.length === 1) {
         cleanMarkup(listEl);
         const markupInfo = createInfoMarkup(data);
@@ -63,24 +49,25 @@ const renderMarkup = data => {
     }
 };
 
-const createListMarkup = data => {
+function createListMarkup(data) {
     return data
         .map(
             ({ name, flags }) =>
                 `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>`,
         )
         .join('');
-};
+}
 
-const createInfoMarkup = data => {
+function createInfoMarkup(data) {
     return data.map(
         ({ name, capital, population, flags, languages }) =>
             `<h1><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official
             }</h1>
-      <p>Capital: ${capital}</p>
-      <p>Population: ${population}</p>
-      <p>Languages: ${Object.values(languages)}</p>`,
+    <p>Capital: ${capital}</p>
+    <p>Population: ${population}</p>
+    <p>Languages: ${Object.values(languages)}</p>`,
     );
 };
+// -----EVENTS-----
 
 inputEl.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,15 +1,29 @@
-const BASE_URL = 'https://restcountries.com/v3.1/name'
+const BASE_URL = 'https://restcountries.com/v3.1/name';
 
 function fetchCountries(name) {
-    fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
-        .then(
-            response => {
-                if (response.status === 404) {
-                    return Promise.reject(new Error());
-                }
-                return response.json();
-            },
-        );
+    return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`).then(
+        response => {
+            if (response.status === 404) {
+                return Promise.reject(new Error());
+            }
+            return response.json();
+        },
+    );
 }
 
-export { fetchCountries }
+export { fetchCountries };
+
+// Напиши функцию fetchCountries(name) которая делает HTTP - запрос на ресурс name и возвращает промис
+//  с массивом стран - результатом запроса.Вынеси её в отдельный файл fetchCountries.js и сделай именованный экспорт.
+
+// Фильтрация полей
+// В ответе от бэкенда возвращаются объекты, большая часть свойств которых тебе не пригодится.Чтобы сократить объем передаваемых данных 
+// добавь строку параметров запроса - так этот бэкенд реализует фильтрацию полей.Ознакомься с документацией синтаксиса фильтров.
+
+// Тебе нужны только следующие свойства:
+
+// name.official - полное имя страны
+// capital - столица
+// population - население
+// flags.svg - ссылка на изображение флага
+// languages - массив языков
